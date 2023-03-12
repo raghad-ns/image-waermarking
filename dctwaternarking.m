df0db31fbec148f5441be7e64ac4b32fb4166e62
raghad-ns <raghadnadersalem20022017@gmail.com>
2023-03-12 11:04:50 +0200

    colored image-watermarking with degradation

--- dctwaternarking.m
+++ dctwaternarking.m
@@ -1,60 +1,43 @@
-% Load the image
+% Load the RGB image and separate its color channels
 img = imread('LenaRGB.jpg');
+R = img(:,:,1);
+G = img(:,:,2);
+B = img(:,:,3);
 
-% Convert the image into grayscale
-gray_img = rgb2gray(img);
+% Convert each color channel into double precision
+R_double = im2double(R);
+G_double = im2double(G);
+B_double = im2double(B);
 
-% convert the image to double precision
-img_double = im2double(gray_img);
+% Compute the DCT coefficients of each color channel
+R_dct = dct2(R_double);
+G_dct = dct2(G_double);
+B_dct = dct2(B_double);
 
-% Apply 2D DCT to the grayscale image
-dct_data = dct2(img_double);
-imshow (dct_data);
-
-% Convert the text to a binary sequence using ASCII codes
+% Load the text to be embedded as a watermark and convert it to a numeric vector of double precision
 text = 'Raghad Dala';
-binary_text = dec2bin(text, 8);
-binary_text = binary_text(:)' - '0';
+text_double = double(text);
 
-% Generate a PRBS with the same length as the binary sequence of the text
-prbs = randi([0 1], 1, length(binary_text));
+% Compute the DCT coefficients of the text
+text_dct = dct(text_double);
 
-% Modulate the binary sequence of the text with the PRBS using XOR operation
-spread_spectrum = xor(binary_text, prbs);
+% Normalize the DCT coefficients of the text to the range [-1, 1]
+text_norm = 2*text_dct/length(text_dct) - 1;
 
-% Compute the absolute values of the DCT coefficients
-abs_dct_data = abs(dct_data);
+% Embed the watermark into the DCT coefficients of the image
+alpha = 0.1; % Watermark strength
+text_norm_resized = imresize(text_norm, [size(R_dct,1), size(R_dct,2)]);
+R_wm_dct = R_dct + alpha* text_norm_resized;
+G_wm_dct = G_dct + alpha* text_norm_resized;
+B_wm_dct = B_dct + alpha* text_norm_resized;
 
-% Apply quantization to the absolute values
-alpha = 0.01; % Adjust alpha to control the watermark strength
-Q = quantiz(abs_dct_data(:), alpha);
+% Apply inverse DCT to obtain the watermarked color channels in the spatial domain
+R_wm = idct2(R_wm_dct);
+G_wm = idct2(G_wm_dct);
+B_wm = idct2(B_wm_dct);
 
-% Reshape the quantized values to the same size as the DCT coefficients
-Q = reshape(Q, size(abs_dct_data));
+% Combine the watermarked color channels to obtain the watermarked RGB image
+img_wm = cat(3, R_wm, G_wm, B_wm);
 
-% Embed the spread spectrum signal into the frequency domain of the image using QIM
-[dct_rows, dct_cols] = size(dct_data);
-for i = 1:dct_rows
-    for j = 1:dct_cols
-        if Q(i, j) == 1 || Q(i, j) == -1
-            dct_data(i, j) = dct_data(i, j) + alpha * sign(abs(dct_data(i, j))) * spread_spectrum(mod(i+j, length(spread_spectrum))+1);
-        end
-    end
-end
-
-imshow (dct_data);
-% Apply inverse 2D DCT to the watermarked image
-watermarked_dct = idct2(dct_data);
-
-% Save the watermarked image
-%imwrite(watermarked_dct, 'watermarked.jpg');
-% Apply inverse DCT to obtain the watermarked image in spatial domain
-watermarked_image_idct = idct2(watermarked_dct);
-
-% Invert the conversion of image to double precision
-img_gray = img_double * 255;
-img_gray = uint8(img_gray);
-
-% Invert the conversion of image to grayscale
-img_rgb = ind2rgb(img_gray, gray(256));
-imshow (img)
+% Display the watermarked image
+imshow(img_wm);
--- dwtwatermarking.m
+++ dwtwatermarking.m
@@ -9,6 +9,7 @@ img_gray = rgb2gray(img);
 [C,S] = wavedec2(img_gray, 1, 'haar');
 [H1,V1,D1] = detcoef2('all',C,S,1);
 A1 = appcoef2(C,S,'haar',1);
+imshow (A1);
 
 % Convert the text to a vector of ASCII codes and apply DWT
 text_arr = double(text);
@@ -29,7 +30,7 @@ D1_modified = reshape(CD1,size(D1));
 % Transform the modified coefficients back to the spatial domain
 C_new = [A1(:)', H1(:)', V1(:)', D1_modified(:)'];
 watermarked_img = waverec2(C_new,S,'haar');
+%imshow(watermarked_img)
 
 % Save the watermarked image
-imwrite(watermarked_img, 'watermarked_image.png');
-imshow (img)
+%imwrite(watermarked_img, 'watermarked_image.png');
