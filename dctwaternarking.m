43633b5dbfc839cc40131044492b063d37059687
raghad-ns <raghadnadersalem20022017@gmail.com>
2023-04-02 13:45:13 +0300

    trying to devide image to blocks then apply cdt

--- dctwaternarking.m
+++ dctwaternarking.m
@@ -31,24 +31,14 @@ R = img(:,:,1);
 G = img(:,:,2);
 B = img(:,:,3);
 
-% Convert each color channel into double precision
-R_double = im2double(R);
-G_double = im2double(G);
-B_double = im2double(B);
-
-% Compute the DCT coefficients of each color channel
-R_dct = dct2(R_double);
-G_dct = dct2(G_double);
-B_dct = dct2(B_double);
-
 % Divide the image into blocks
-[numRows, numCols] = size(R_dct);
+[numRows, numCols] = size(R);
 numBlocksRows = floor(numRows / blockSize);
 numBlocksCols = floor(numCols / blockSize);
 
-blocks_R = mat2cell(R_dct, blockSize*ones(1,numBlocksRows), blockSize*ones(1,numBlocksCols), 1);
-blocks_G = mat2cell(G_dct, blockSize*ones(1,numBlocksRows), blockSize*ones(1,numBlocksCols), 1);
-blocks_B = mat2cell(B_dct, blockSize*ones(1,numBlocksRows), blockSize*ones(1,numBlocksCols), 1);
+blocks_R = mat2cell(R, blockSize*ones(1,numBlocksRows), blockSize*ones(1,numBlocksCols), 1);
+%blocks_G = mat2cell(G_dct, blockSize*ones(1,numBlocksRows), blockSize*ones(1,numBlocksCols), 1);
+%blocks_B = mat2cell(B_dct, blockSize*ones(1,numBlocksRows), blockSize*ones(1,numBlocksCols), 1);
 
 figure;
 title('Host Image')
@@ -66,7 +56,6 @@ text_double = double(text);
 
 % Convert the watermark to binary data
 binWatermark = dec2bin(uint8(text), 8);
-
 
 % Reshape the binary data into array 
 binWatermark = reshape(binWatermark', [], 1);
@@ -91,9 +80,14 @@ watermarkIndex = 1;
 % Main code starts here
 for i = 1:numBlocksRows
     for j = 1:numBlocksCols
-        block_R = blocks_R{i,j};
-        block_G = blocks_G{i,j};
-        block_B = blocks_B{i,j};
+        % Convert red color channel into double precision
+        R_double = im2double(blocks_R{i , j});
+
+        % Compute the DCT coefficients of red color channel
+        R_dct = dct2(R_double);
+        block_R = R_dct;
+        %block_G = blocks_G{i,j};
+        %block_B = blocks_B{i,j};
         block_R_int = uint8(block_R); % Cast to integer type
         for k = 1:blockSize
             for l = 1:blockSize
@@ -106,6 +100,8 @@ for i = 1:numBlocksRows
                 end
             end
         end
+        block_R_idct = idct2(block_R);
+        blocks_R{i,j} = block_R_idct;
     end
 end
 
@@ -120,17 +116,17 @@ for i = 1:numBlocksRows
 end
 
 % Combine the blocks back into an image
-R_dct = cell2mat(blocks_R);
-G_dct = cell2mat(blocks_G);
-B_dct = cell2mat(blocks_B);
+R = cell2mat(blocks_R);
+%G_dct = cell2mat(blocks_G);
+%B_dct = cell2mat(blocks_B);
 
 % Compute the inverse DCT of each color channel
-R_idct = idct2(R_dct);
-G_idct = idct2(G_dct);
-B_idct = idct2(B_dct);
+%R_idct = idct2(R_dct);
+%G_idct = idct2(G_dct);
+%B_idct = idct2(B_dct);
 
 % Combine the color channels into an RGB image
-img_wm = cat(3, R_idct, G_idct, B_idct);
+img_wm = cat(3, R, G, B);
 
 % Remove the padding
 img_wm = img_wm(1:end-padRows, 1:end-padCols, :);
@@ -138,6 +134,58 @@ img_wm = img_wm(1:end-padRows, 1:end-padCols, :);
 figure;
 imshow(img_wm);
 imwrite(img_wm, 'output.jpg', 'jpg');
+
+%extraction code 
+% Initialize variables for the extracted watermark
+extractedWatermark = '';
+keyIndex = 2;
+
+% Iterate over each block
+for i = 1:numBlocksRows
+    for j = 1:numBlocksCols
+        block_R = blocks_R{i,j};
+        
+        for k = 1:blockSize
+            for l = 1:blockSize
+                if keyIndex <= numel(numKey) && numKey(keyIndex) == k && numKey(keyIndex - 1) == l
+                    % Extract the least significant bit from the current pixel value in block_R
+                    block_R_int = uint8(block_R); % Cast to integer type
+                    watermarkBit = bitget(uint8(block_R_int(k, l)), 1);
+                    % Append the watermark bit to the extractedWatermark string
+                    extractedWatermark = strcat(extractedWatermark, num2str(watermarkBit));
+                    % Update keyIndex
+                    keyIndex = keyIndex + 2;
+                end
+            end
+        end
+    end
+end
+
+% Convert the binary string to the original watermark message
+%extractedMessage = binaryVectorToASCII(extractedWatermark);
+
+disp(extractedWatermark);
+disp(size(extractedWatermark));
+
+function text = bin2text(binaryStr)
+    % Convert the binary string to a character array
+    binaryArray = reshape(binaryStr, 8, [])';
+    
+    % Convert each row of the binary array to a decimal value
+    decimalValues = bin2dec(binaryArray);
+    
+    % Convert the decimal values to characters
+    text = char(decimalValues)';
+end
+
+function asciiMsg = binaryVectorToASCII(binaryMsg)
+    % Convert the binary message to a matrix
+    binaryMat = reshape(binaryMsg, 8, length(binaryMsg)/8)';
+    % Convert each row of the binary matrix to decimal
+    decimalMat = bi2de(binaryMat);
+    % Convert the decimal matrix to ASCII
+    asciiMsg = char(decimalMat)';
+end
 
 
 % function definition for modify_bit
